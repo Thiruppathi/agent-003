e7b3da0d546824adb9002638158f6de0db9988c4
Sander Alberink <alberink@gmail.com>
2025-10-27 19:53:22 +0100

    Add subdirectory

--- elderly-care/agent.py
+++ elderly-care/agent.py
@@ -1,8 +1,8 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
+    model='gemini-flas',
     name='root_agent',
-    description='An elderly care agent to suport',
+    description='An elderly care agent to support older people living alone',
     instruction='Answer user questions to the best of your knowledge',
 )
