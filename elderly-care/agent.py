b42c86c2134aa83f6a1e81272f99be911b537754
Sander Alberink <alberink@gmail.com>
2025-10-27 20:27:14 +0100

    Add live streaming

--- elderly-care/agent.py
+++ elderly-care/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='gemini-flas',
+    model='gemini-live-2.5-flash-preview-native-audio-09-2025',
     name='root_agent',
     description='An elderly care agent to support older people living alone',
     instruction='Answer user questions to the best of your knowledge',
